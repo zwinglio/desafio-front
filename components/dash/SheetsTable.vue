0e4b20a46064da83736f38bd74bab141f862ce01
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-04-28 13:57:03 -0300

    change css to import fonts and incons

--- components/dash/SheetsTable.vue
+++ components/dash/SheetsTable.vue
@@ -58,11 +58,6 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      sheets: [],
-    };
-  },
   methods: {
     destroySheet(id) {
       //alert user
--- layouts/default.vue
+++ layouts/default.vue
@@ -21,4 +21,8 @@
 
 <script></script>
 
-<style scoped></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;400;800&family=Poppins:wght@100;400;700&display=swap");
+
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css");
+</style>
