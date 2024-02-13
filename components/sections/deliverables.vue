a4006d952e7d6e87457e4e759bcb4c133778ead4
Samuel Zwinglio <samuel.azevedo@cloudplusplus.nl>
2024-02-13 19:07:30 -0300

    update link and add a time validator

--- components/sections/deliverables.vue
+++ components/sections/deliverables.vue
@@ -1,40 +1,44 @@
 <template>
     <section id="entregaveis">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-8">
-                <h2>Seu treino no seu WhatsApp</h2>
-                <hr>
-                <h3 class="mt-5">O que você vai receber?</h3>
-                <ul>
-                    <li>Uma planilha de treinos <strong>toda semana</strong> de acordo com seu nível;</li>
-                    <li>Vídeos explicativos para cada treino na planilha;</li>
-                    <li>Sugestão de cardápio para melhorar sua alimentação nesses 30 dias;</li>
-                    <li>Suporte de dúvidas diretamente com a personal Carina Rosin.</li>
-                </ul>
-                <h4 class="fw-bold text-warning py-4">Por apenas 99 centavos por dia você tem acesso completo ao desafio!
-                </h4>
-                <p>O desafio tem uma duração de 30 dias. Você não vai ter nem tempo de pensar em desistir. Aqui o
-                    objetivo é te levar ao teu limite e te provar que você é capaz de transformações incríveis na sua
-                    rotina sem precisar sacrificar muito espaço da sua vida. </p>
-                <div class="w-100 text-center my-3">
-                    <NuxtPicture
-                        src="/img/landing/preco.png"
-                        alt=""
-                        format="webp"
-                        loading="lazy"
-                    />
-                                    </div>
-                <div class="countdown w-100 text-center bg-warning text-dark fw-bold py-4 px-3 rounded-3">As inscrições começaram na quarta-feira de cinzas!</div>
-                                    <a type="button" href="#modalCheckout" class="btn btn-lg btn-danger w-100 py-3 mt-4" data-bs-toggle="modal">Quero fazer parte!</a>
-                            </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <h2>Seu treino no seu WhatsApp</h2>
+                    <hr>
+                    <h3 class="mt-5">O que você vai receber?</h3>
+                    <ul>
+                        <li>Uma planilha de treinos <strong>toda semana</strong> de acordo com seu nível;</li>
+                        <li>Vídeos explicativos para cada treino na planilha;</li>
+                        <li>Sugestão de cardápio para melhorar sua alimentação nesses 30 dias;</li>
+                        <li>Suporte de dúvidas diretamente com a personal Carina Rosin.</li>
+                    </ul>
+                    <h4 class="fw-bold text-warning py-4">Por apenas 99 centavos por dia você tem acesso completo ao
+                        desafio!
+                    </h4>
+                    <p>O desafio tem uma duração de 30 dias. Você não vai ter nem tempo de pensar em desistir. Aqui o
+                        objetivo é te levar ao teu limite e te provar que você é capaz de transformações incríveis na sua
+                        rotina sem precisar sacrificar muito espaço da sua vida. </p>
+                    <div class="w-100 text-center my-3">
+                        <NuxtPicture
+                            src="/img/landing/preco.png"
+                            alt=""
+                            format="webp"
+                            loading="lazy"
+                        />
+                    </div>
+                    <div class="countdown w-100 text-center bg-warning text-dark fw-bold py-4 px-3 rounded-3">As inscrições
+                        começaram na quarta-feira de cinzas!</div>
+                    <a
+                        href="#modalCheckout"
+                        class="btn btn-lg btn-danger w-100 py-3 mt-4"
+                        data-bs-toggle="modal"
+                    >Começar agora!</a>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 </template>
 
-<style lang="css">
-#entregaveis {
-  background-color: #060201;
-}
-</style>
+<style lang="css">#entregaveis {
+    background-color: #060201;
+}</style>
---
+++ components/sections/modal-checkout.vue
@@ -0,0 +1,110 @@
+<template>
+    <div
+        class="modal fade"
+        id="modalCheckout"
+        tabindex="-1"
+        aria-labelledby="modalCheckout"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header align-items-center">
+                    <span class="fs-3">🔥 </span>
+                    <h3
+                        class="modal-title text-dark text-uppercase fs-5"
+                        id="modalLabel"
+                    >Falta pouco!</h3>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body text-dark">
+                    <div v-if="!isTimeToShowForm">
+                        <p>Você chegou cedo!</p>
+                        <p>As inscrições começam dia 14/02 (amanhã) as 8h da manhã ☀️</p>
+                    </div>
+                    <form
+                        id="formModal"
+                        @submit.prevent="submit"
+                        v-if="isTimeToShowForm"
+                    >
+                        <label
+                            for="name"
+                            class="mt-3"
+                        >Nome:</label>
+                        <input
+                            type="text"
+                            class="form-control form-control-lg"
+                            name="name"
+                            v-model="name"
+                        >
+                        <label
+                            for="name"
+                            class="mt-3"
+                        >E-mail principal:</label>
+                        <input
+                            type="text"
+                            class="form-control form-control-lg"
+                            name="email"
+                            v-model="email"
+                        >
+                        <label
+                            for="name"
+                            class="mt-3"
+                        >Número do WhatsApp:</label>
+                        <input
+                            type="text"
+                            class="form-control form-control-lg"
+                            name="phone"
+                            v-model="phone"
+                        >
+                        <button
+                            type="submit"
+                            class="btn btn-success w-100 fw-bold text-uppercase py-3 btn-send mt-4"
+                        >Continuar para
+                            o Desafio!</button>
+                    </form>
+                </div>
+                <div
+                    class="modal-footer"
+                    v-if="isTimeToShowForm"
+                >
+                    <p class="text-right text-muted small"><span>Se preferir, <a
+                                class="text-muted"
+                                :href="checkoutUrl"
+                                target="_blank"
+                            >clique
+                                aqui</a>.</span></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const checkoutUrl = 'https://pay.hotmart.com/C68476388Q?off=9kxkrmd2'
+
+const name = ref("")
+const email = ref("")
+const phone = ref("")
+
+const submit = () => {
+
+    const checkoutUrl = `https://pay.hotmart.com/C68476388Q?off=9kxkrmd2&name=${name.value}&email=${email.value}&phone=${phone.value}`
+
+    window.open(checkoutUrl, '_blank')
+}
+
+const isTimeToShowForm = computed(() => {
+    const targetDateTime = new Date('2024-02-14T08:00:00');
+    const now = new Date();
+
+    // Check if the current date is February  14,  2024, and the time is  08:00 AM
+    return now > targetDateTime;
+});
+</script>
