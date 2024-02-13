d6006e11f576f156dce6d44d760a777c41de1271
Samuel Zwinglio <samuel.azevedo@cloudplusplus.nl>
2024-02-13 18:32:12 -0300

    fix some informations and add the new price

--- components/sections/deliverables.vue
+++ components/sections/deliverables.vue
@@ -9,16 +9,21 @@
                 <ul>
                     <li>Uma planilha de treinos <strong>toda semana</strong> de acordo com seu nível;</li>
                     <li>Vídeos explicativos para cada treino na planilha;</li>
-                    <li>Sugestão de cardápio para melhorar sua alimentação nesses 40 dias;</li>
+                    <li>Sugestão de cardápio para melhorar sua alimentação nesses 30 dias;</li>
                     <li>Suporte de dúvidas diretamente com a personal Carina Rosin.</li>
                 </ul>
                 <h4 class="fw-bold text-warning py-4">Por apenas 99 centavos por dia você tem acesso completo ao desafio!
                 </h4>
-                <p>O desafio tem uma duração de 40 dias. Você não vai ter nem tempo de pensar em desistir. Aqui o
+                <p>O desafio tem uma duração de 30 dias. Você não vai ter nem tempo de pensar em desistir. Aqui o
                     objetivo é te levar ao teu limite e te provar que você é capaz de transformações incríveis na sua
                     rotina sem precisar sacrificar muito espaço da sua vida. </p>
                 <div class="w-100 text-center my-3">
-                                            <img src="https://voceemforma.net/img/desafio/preco.png" alt="">
+                    <NuxtPicture
+                        src="/img/landing/preco.png"
+                        alt=""
+                        format="webp"
+                        loading="lazy"
+                    />
                                     </div>
                 <div class="countdown w-100 text-center bg-warning text-dark fw-bold py-4 px-3 rounded-3">As inscrições começaram na quarta-feira de cinzas!</div>
                                     <a type="button" href="#modalCheckout" class="btn btn-lg btn-danger w-100 py-3 mt-4" data-bs-toggle="modal">Quero fazer parte!</a>
